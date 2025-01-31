d9d1e42bf8dcdda63021dbb2df0e7446f09ee25f
aslanmyn <mustafaev.aslan@55gmail.com>
2025-01-31 16:49:50 +0500

    ToDoapp

---
+++ ToDo_app/todoapp/todolist/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from .database import Base
+
+
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
