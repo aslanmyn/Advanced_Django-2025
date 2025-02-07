ad1748d4662d7f4820ea05cc7099682fa2c62ba8
aslanmyn <mustafaev.aslan@55gmail.com>
2025-02-07 10:26:48 +0500

    +

--- Practice1_2/task_management/urls.py
+++ Practice1_2/task_management/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index
+from Practice1_2.core.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
